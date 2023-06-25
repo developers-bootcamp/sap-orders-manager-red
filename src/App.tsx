2dc943f0b86e6d8e86d6d05c39ff408c33d3c6aa
123yael <119128695+123yael@users.noreply.github.com>
2023-06-25 16:37:40 +0300

    finish login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route , Link} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from 'react-router';
 import LandingPage from "./pages/landing";
-import Login from "./pages/login";
-
-// import SignUpComp from './components/signup';
+import { LogIn } from './pages/login';
 
 function App() {
   return (
-
     <BrowserRouter>
-    
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<LogIn />} />
       </ Routes>
     </BrowserRouter>
   );
