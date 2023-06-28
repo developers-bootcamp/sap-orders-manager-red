524d62069a63cf51adaba20716874405a6869a79
Elisheva1234 <E39857@gmail.com>
2023-06-28 09:53:29 +0300

    28.06

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -11,17 +11,6 @@ import useStyles from './signUp.styles'
 import giftsImg from "../img/gifts.png"
 import SingUpForm from './SignUpForm';
 
-const schema = Yup.object().shape({
-  fullName: Yup.string().required('Name is a required field').max(20, 'You cannot enter more than 20 letters'),
-  companyName: Yup.string().required('Company name is a required field').max(20, 'You cannot enter more than 20 letters'),
-  password: Yup.string()
-    .required('Password is a required field')
-    .min(8, 'Password must be at least 8 characters').max(20, 'You cannot enter more than 20 letters'),
-  email: Yup.string().required('Email is a required field').matches(/^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/
-    , 'Invalid email format').max(200, 'You cannot enter more than 200 letters'),
-  agree: Yup.boolean().required("Required field").oneOf([true], "Required field")
-});
-
 const SignUpComp: React.FC = () => {
   const [open, setOpen] = React.useState(false);
   const classes = useStyles();
