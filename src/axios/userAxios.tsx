1e5e54b4c5f3e5ed28442dca8acff2e422c1f7ff
ChaniDonat <136927748+ChaniDonat@users.noreply.github.com>
2023-07-02 10:45:49 +0300

    Revert "final axios interceptor"
    This reverts commit 04f9e64ce15ebfd5614c98046b662db7a5486ef8.

--- src/axios/userAxios.tsx
+++ src/axios/userAxios.tsx
@@ -1,14 +1,12 @@
 import axios from "axios"
 import { LOG_IN } from "../config/config"
-import './globalAxios'
 
 console.log(LOG_IN);
 
 export const logIn = (email: String, password: String) => {
     console.log(LOG_IN);
     
+    debugger
     console.log(email + " " + password);
-    return axios.get("http://localhost:8080/User/err");
-    // return axios.get(`${LOG_IN}/${email}/${password}`);
-
+    return axios.get(`${LOG_IN}/${email}/${password}`)
 }
