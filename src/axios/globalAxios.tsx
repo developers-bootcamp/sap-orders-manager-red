65e5d67a4bdabd2cf13067d0e002c354bc50e2fb
ChaniDonat <136927748+ChaniDonat@users.noreply.github.com>
2023-07-02 10:05:54 +0300

    Update the userToken variable on each call.

--- src/axios/globalAxios.tsx
+++ src/axios/globalAxios.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { LOG_IN } from "../config/config";
-let userToken = localStorage.getItem("userToken");
+
 
 axios.interceptors.request.use(
     (config: any) => {
+        let userToken = localStorage.getItem("userToken");
         if (config.url.indexOf(LOG_IN) != 0 && userToken) {
-            config.headers["Authorization"] = 'Bearer' + userToken
+            config.headers["token"] = userToken;
         }
         console.log(config);
         return config;
@@ -17,12 +18,12 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
     (response: any) => {
         console.log("I come interceptore response");
+        console.log(response);
         return response;
     },
     (error: any) => {
         if (error.response.status != 401)
             alert("ארע שגיאה אנא פנה למהנל המערכת");
-        else
-            return Promise.reject(error);
+        return Promise.reject(error);
     }
 );
