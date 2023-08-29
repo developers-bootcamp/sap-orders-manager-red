24b356dc279c01b1521f96e958207c5e2b55b632
123yael <119128695+123yael@users.noreply.github.com>
2023-08-29 13:11:07 +0300

    after merge

--- src/pages/filterPop/AllFilter.tsx
+++ src/pages/filterPop/AllFilter.tsx
@@ -1,31 +1,31 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { Button } from "@mui/material";
-import { PALLETE} from "../../config/config";
+import { PALLETE } from "../../config/config";
 import FilterPop from "./FilterPop";
 
-const AllFilter = (props:any) => {
+const AllFilter = (props: any) => {
   const [filters, setFilters] = useState([{ fieldName: "status", filterValue: "DONE" },]);
-const {filterTables}=props;
+  const { filterTables } = props;
   const addFilter = () => {
     setFilters((prevFilters) => [
       ...prevFilters,
       { fieldName: "status", filterValue: "DONE" },
     ]);
   };
-  const changeFieldName =async (filter: string, index: number) => {
+  const changeFieldName = async (filter: string, index: number) => {
     const updatedFilters = [...filters];
     updatedFilters[index] = { ...updatedFilters[index], fieldName: filter };
-    setFilters(updatedFilters);   console.log("f",filters);
+    setFilters(updatedFilters); console.log("f", filters);
   };
-  const changeFilterValue =async (filter: string, index: number) => {
+  const changeFilterValue = async (filter: string, index: number) => {
     const updatedFilters = [...filters];
     updatedFilters[index] = { ...updatedFilters[index], filterValue: filter };
     setFilters(updatedFilters);
   };
 
   const arrayToDictionary = () => {
-    return filters.reduce((dictionary:any, item) => {
+    return filters.reduce((dictionary: any, item) => {
       dictionary[item.fieldName] = item.filterValue;
       return dictionary;
     }, {});
@@ -33,9 +33,9 @@ const {filterTables}=props;
   const filterOrder = () => {
     filterTables(arrayToDictionary());
   }
-  useEffect(()=>{
-    console.log("filters",filters);
-  },[filters])
+  useEffect(() => {
+    console.log("filters", filters);
+  }, [filters])
   return (
     <>
       {filters.map((filter, index) => (
