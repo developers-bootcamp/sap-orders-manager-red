c5a818e119a806a8a4d51349251349b9d72e1458
Ruti Churi <r0527694893@gmail.com>
2023-09-04 10:57:45 +0300

    edit work well

--- src/pages/tables/UserTable.tsx
+++ src/pages/tables/UserTable.tsx
@@ -8,6 +8,7 @@ import ArrowCircleUpSharp from '@mui/icons-material/ArrowCircleUpSharp';
 import IAddress from "../../interfaces/IAddress";
 import { Email } from "@mui/icons-material";
 import IUserDTO from "../../interfaces/IUserDTO";
+import { setDefaultResultOrder } from "dns";
 
 
 const UserTable = () => {
@@ -16,7 +17,9 @@ const UserTable = () => {
     const [isOpenEmp, setIsOpenEmp] = useState(true);
     const [isOpenCustomer, setIsOpenCustomer] = useState(true);
     const[change,setChange]=useState(false);
-
+    const[disable,setDisable]=useState(true);
+    const[error,setError]=useState(false)
+    const [errorMessage,setErrorMessage]=useState(" ")
 
     const getAllUserAsync = async (pageNumber: number) => {
         await getAllUser(pageNumber).then(res =>{ setAllUser(res.data);console.log(res.data)});
@@ -28,36 +31,42 @@ const UserTable = () => {
         setChange(false);       
     },[change]);
 
-    const goToEditUser = async (id: string, user: {FullName?: string,Password?: string, Phone?:string,Email?:string,Address?:string,RoleId?:string ,fullName:string,password:string,phone:string,email:string,address:string,roleId:string}) => {
+    const goToEditUser = async (user: { FullName: string, Password: string, Phone: string, Email: string, Address: string, id: string}) => {
         const editUser1: IUserDTO = {
-            fullName:user.FullName || user.fullName,
-            password:user.Password ||user.password,
-            phone:user.Phone ||user.phone,
-            address:user.Address || user.address,
-            email:user.Email ||user.email,
-            roleId:user.RoleId ||user.roleId
+            fullName: user.FullName,
+            password: user.Password,
+            phone: user.Phone,
+            address: user.Address,
+            email: user.Email,
         }
-        editUser(id, editUser1);
+        editUser(user.id, editUser1);
         setChange(true);
     }
-
-
+debugger
     const goToAddAdmin = async (user: { FullName: string, Password: string,Phone:string,Email:string,Address:string }) => {
+        
         const addUser1: IUserDTO = {
             fullName:user.FullName,
             password:user.Password,
             phone:user.Phone,
             address:user.Address,
             email:user.Email,
-            roleId:"1"
-            
+            roleId:"1"    
         }
         console.log(addUser1)
-        await addUser(addUser1)
+        await addUser(addUser1).catch(error=>{
+            setError(true);
+            if(error.code=='UnsupportedOperationException'){
+                setErrorMessage('cannot add Administrator')
+            }
+            else{
+                setErrorMessage('the request cannot be complete please try again')
+            }
+        });
+       
         setChange(true);
 
     }
-  
 
     const goToAddEmp = async (user: { FullName: string, Password: string,Phone:string,Email:string,Address:string }) => {
         const addUser2: IUserDTO = {
@@ -93,7 +102,7 @@ const UserTable = () => {
         setChange(true);
     }
 
-
+    
     const head =
     [{ name: "FullName", type: "text" },
      { name: "Password", type: "text" },
@@ -115,15 +124,15 @@ const UserTable = () => {
             {allUser != null ? <GlobalTable head={head} rows={allUser.filter(user=>user.roleId === "1")
         .map(user=>({
             id: user.id,
-            FullName:user.fullName,
-            Password:"*********",
-            Email:user.email,
-            Address:user.address,
-            Phone:user.phone
+            fullName:user.fullName,
+            password:"*********",
+            email:user.email,
+            address:user.address,
+            phone:user.phone
         }))
         
         
-        } whatToAdd="Administrator" delete={goToDeleteUser} add={goToAddAdmin} edit={goToEditUser}></GlobalTable> : ""}
+        } whatToAdd="Administrator" delete={goToDeleteUser} add={ goToAddAdmin} edit={goToEditUser}></GlobalTable> : ""}
         </div>    }
             <br></br>
             <Button sx={{ color: PALLETE.YELLOW }} startIcon={<ArrowCircleUpSharp />} onClick={() => setIsOpenEmp(!isOpenEmp)}>{isOpenEmp ? 'Employees' : 'Employees'}
@@ -134,12 +143,11 @@ const UserTable = () => {
 <div>            {allUser != null ? <GlobalTable head={head} rows={allUser.filter(user=>user.roleId === "2")
 .map(user=>({
     id: user.id,
-    FullName:user.fullName,
-    Password:"*********",
-    Email:user.email,
-    Address:user.address,
-    Phone:user.phone
-
+    fullName:user.fullName,
+    password:"*********",
+    email:user.email,
+    address:user.address,
+    phone:user.phone
 }))
 } whatToAdd="Employee" delete={goToDeleteUser} add={goToAddEmp} edit={goToEditUser}></GlobalTable> : ""}
 
@@ -153,13 +161,12 @@ const UserTable = () => {
 <div>            
 {allUser != null ? <GlobalTable head={head} rows={allUser.filter(user=>user.roleId === "3")
 .map(user=>({
-    id: user.id,
-    FullName:user.fullName,
-    Password:"*********",
-    Email:user.email,
-    Address:user.address,
-    Phone:user.phone
-
+     id: user.id,
+            fullName:user.fullName,
+            password:"*********",
+            email:user.email,
+            address:user.address,
+            phone:user.phone
 }))
 } whatToAdd="Customer" delete={goToDeleteUser} add={goToAddCust} edit={goToEditUser}></GlobalTable> : ""}
 
